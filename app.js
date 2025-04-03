b63b5e23b02de47587d92cefdc38b40f7223f470
amitjomy007 <amitjomy007@gmail.com>
2025-04-03 21:15:59 +0530

    middleware Funny Visualization

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const app = express(); // express is now ready to be used
 app.set('view engine', 'ejs'); // other alternativs for ejs pugs, jade 
 
 
+//middle ware : visualize it in action
+//this middle ware will perform the following when a user tries to access any route, any route, whatever it is...!
+app.use((req,res) => {
+    console.log("user trying to access a route, whatever route it is. Now I middle ware will decide what to do");
+    res.send("Haha, im middleware, im not going to send you to your desired route, you will be stuck and rot here, all say hail middleware, cause i decide what to do and where you should be sent");
+})
+
 
 app.get('/', (req,res) => {
     // res.send("Haha you are in '/' route");

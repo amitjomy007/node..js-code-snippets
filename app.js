a8036579c3b30c17a27ab4e1a25d11177f442b0f
amitjomy007 <amitjomy007@gmail.com>
2025-04-03 20:39:54 +0530

    routing basic example

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const http = require('http');
 
 //creates a server, callback whenever there is a request and executes res
 const server = http.createServer((req,res) => { 
-    res.end('Hello world  ');
+    
+
+    console.log(req.url); // example : localhost:3000/about this line of code will print /about
+    //so how to route?
+
+    if(req.url=="/about"){
+        res.end("You are in the about page");
+    }
+    else {
+        res.end("Error 404 not found");
+    }
     
 });
 //whenever user visits website Hello world will appear 

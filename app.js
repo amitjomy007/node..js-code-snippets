d1bbfa5bb53b155423f6a7ceced38c921fbc081a
amitjomy007 <amitjomy007@gmail.com>
2025-04-03 21:23:37 +0530

    middleware, now allowing user to go to route using next()

--- app.js
+++ app.js
@@ -9,10 +9,20 @@ app.set('view engine', 'ejs'); // other alternativs for ejs pugs, jade
 
 //middle ware : visualize it in action
 //this middle ware will perform the following when a user tries to access any route, any route, whatever it is...!
-app.use((req,res) => {
-    console.log("user trying to access a route, whatever route it is. Now I middle ware will decide what to do");
-    res.send("Haha, im middleware, im not going to send you to your desired route, you will be stuck and rot here, all say hail middleware, cause i decide what to do and where you should be sent");
+app.use((req,res,next) => {
+    
+    console.log("You are in middleware now");
+    
+    //your middleware Logic can be written here if any
+    const a = 2;
+    const b = 3;
+    console.log(a+b);
+
+    //now finally let the user go to their desired route
+    return next();
 })
+
+//apologies : I forgot to add next parameter in previous  commit, it should be there sorry.
 
 
 app.get('/', (req,res) => {

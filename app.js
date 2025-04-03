f92bc3909dc42031ae2ef687c21d881f7e37a097
amitjomy007 <amitjomy007@gmail.com>
2025-04-03 19:50:45 +0530

    create a server and send a msg

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-//intial commit
+const http = require('http');
+
+
+//creates a server, callback whenever there is a request and executes res
+const server = http.createServer((req,res) => { 
+    res.end('Hello world  ');
+    
+});
+//whenever user visits website Hello world will appear 
+
+
+//create the server at port 3000
+server.listen(3000);
